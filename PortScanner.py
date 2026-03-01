da9a7fe0877db198fd81b1c6ae317475ea7990bd
aishwary <aishwarysaxena2006@gmail.com>
2026-03-02 01:26:21 +0530

    vulnScanner file setup

---
+++ PortScanner.py
@@ -0,0 +1,42 @@
+import socket
+from IPy import IP
+
+from vulnScanner import targets_ip
+
+
+class PortScan():
+    banners=[]
+    open_ports=[]
+    def __init__(self,target,port_num):
+        self.target=targets_ip
+        self.port_num=port_num
+    def scan(self):
+        #converted_id=check_ip(target)
+        #print('\n' + "[-_0 Scanning Target]"+str(target))
+        for port in range(1,500):
+            self.scan_port(port)
+
+    def check_ip(self):
+        try:
+            IP(self.target)
+            return (self.target)
+        except ValueError:
+            return socket.gethostbyname(ip)
+
+    def get_banner(s):
+        return.recv(1024)
+    def scan_prt(selfself,port):
+        try:
+            converted_ip=self.check_ip()
+            sock=socket.socket()
+            sock.settimeout(0.5)
+            sock.connect((converted_ip,port))
+            self.open_ports.append(port)
+            try:
+                banner=sock.recv(1024).decode().strip('\n').strip('\r')
+                self.banenrs.append(banner)
+            except:
+                self.banners.append(' ')
+            sock.close()
+        except:
+            pass
---
+++ vulnScanner.py
@@ -0,0 +1,8 @@
+import PortScanner
+
+targets_ip=iput('[+] * Enter Target To Scan For Vulnerable Open Ports:  ')
+Port_number=int(input('[+] * Enter Amount of Ports You Want To Scan(500- First 500 ports): '))
+vul_file=input('[+] * Enter Vulnerable File Path: ')
+print('\n')
+
+PortScanner.scan(targets_ip)
